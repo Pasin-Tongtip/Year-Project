f592d9f8adee134605970d960964c3795c3bbc3a
Pasin-Tongtip <pasin.to@ku.th>
2025-05-11 19:23:52 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 class Config:
     __ALL_CONFIGS = {
-        'WIN_SIZE_W': 800,
-        'WIN_SIZE_H': 600,
-        'GRID_SIZE': 20,
-        'GRID_COUNT_W': 40,
-        'GRID_COUNT_H': 30,
-        'BLACK': (0, 0, 0),
-        'GREEN': (0, 255, 0),
-        'RED': (255, 0, 0),
+        'WIDTH': 800,
+        'HEIGHT': 600,
         'WHITE': (255, 255, 255),
-        'LIGHT_GREY': (200, 200, 200),
+        'BLACK': (0, 0, 0),
+        'RED': (255, 0, 0),
+        'GREEN': (0, 255, 0),
+        'BLUE': (50, 153, 213),
+        'BLOCK_SIZE': 20,
+        'SPEED': 10,
+        'NUM_FOOD': 5,
     }
 
     @classmethod
